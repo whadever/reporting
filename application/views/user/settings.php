b5fa08f26ce3e1964e507ab9e78c9fd3017730f4
setyawan9 <setyawansusanto99@outlook.com>
2016-09-15 10:09:20 +0700

    update settings working

--- application/views/user/settings.php
+++ application/views/user/settings.php
@@ -32,49 +32,57 @@
 	</div>
 </div>
 
+<pre>
+	<?php print_r($users) ?>
+</pre>
+
 <div class="row" style="padding:15px 10px">
 	<div class="col-xs-12">
 		<div class="container">
+		<?php echo form_open_multipart('user/save_settings/'.$users->id) ?>
 		<div class="row">
 			<div class="col-xs-3 col-sm-2 col-md-1">
-				<div class="avatar" style="background-color : grey ; ">
+				<div class="avatar" style="background-image : url(<?php echo base_url().$users->photo ?>); ">
 				</div>
 				
 			</div>
 			<div class="col-xs-9 col-sm-10 col-md-11">
 				<strong><p>Change Profile Picture</p>
-				<input type="file"></strong>
+				<input type="file" name="photo"></strong>
 			</div>
 		</div>
+		
 		<div class="row" id="fields" style="padding-top:20px">	
+			
 			<div class="col-md-6">
 				<div class="form-group">	
 					<label for="">Name:</label>
-					<input type="text" class="form-control">
+					<input type="text" class="form-control" name="name" value="<?php echo $users->name ?>">
 				</div>
 				<div class="form-group">	
 					<label for="">Address:</label>
-					<input type="text" class="form-control">
+					<input type="text" class="form-control" name="address" value="<?php echo $users->address ?>">
 				</div>
 				<div class="form-group">	
 					<label for="">Phone No.:</label>
-					<input type="text" class="form-control">
+					<input type="text" class="form-control" name="phone_number" value="<?php echo $users->phone_number ?>">
 				</div>
 				<div class="form-group">	
 					<label for="">Mobile No.:</label>
-					<input type="text" class="form-control">
+					<input type="text" class="form-control" name="mobile_number" value="<?php echo $users->mobile_number ?>">
 				</div>
 			</div>
 			<div class="col-md-6">
 				<div class="form-group">	
-					<label for="">Usename:</label>
-					<input type="text" class="form-control">
+					<label for="">Username:</label>
+					<input type="text" class="form-control" name="username" value="<?php echo $users->username ?>" disabled="disabled">
 				</div>
 				<div class="form-group">	
 					<label for="">Password:</label>
 					<a href="">Change Password</a>
 				</div>
 			</div>
+
 		</div>
 		<div class="row" style="padding-top:50px;">	
 			<div class="col-md-4">	</div>
@@ -84,12 +92,13 @@
 						<a href="	" class="btn btn-primary form-control" style="border-radius:10px;">BACK</a>
 					</div>
 					<div class="col-md-6">	
-						<a href="	" class="btn btn-primary form-control" style="border-radius:10px;">SAVE</a>
+						<input type="submit" name="save" value="SAVE" class="btn btn-primary form-control" style="border-radius:10px;">
 					</div>
 				</div>
 			</div>
 			<div class="col-md-4">	</div>
 		</div>
+		<?php echo form_close() ?>
 			
 		</div>
 	</div>
