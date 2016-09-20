7790ffec3b08ae99d8637ace87f140168346a665
whadever <irpanwinata@gmail.com>
2016-09-20 11:09:33 +0700

    get rekt sultan

--- application/models/Calendar_model.php
+++ application/models/Calendar_model.php
@@ -7,6 +7,7 @@
 		function __construct(){
 			parent::__construct();
 
+			//calendar config
 			$this->conf = array(
             'start_day' => 'sunday',
             'show_next_prev' => true,
@@ -14,6 +15,7 @@
             'day_type' => 'short'
             );
 
+			//calendar template
 			$this->conf['template'] = '
 				{table_open}<table border="0" cellpadding="0" cellspacing="0">{/table_open}
 
@@ -58,6 +60,86 @@
 
 		}
 
+		//generate calendar for manager
+		function manager_generate($year,$month,$company_id,$user_id){
+
+	        $this->load->library('calendar',$this->conf);
+
+	        $this->db->select('form_submits.*,forms.name as report_name,forms.frequency,forms.report_color,users.name,users.id as user_id,users.role');
+	        $this->db->from('form_submits');
+	        $this->db->join('forms','forms.id = form_submits.form_id');
+	        $this->db->join('users', 'users.id = form_submits.user_id');
+	        $this->db->where('users.company_id',$company_id);
+
+
+	        $user_reports = $this->db->get()->result();
+
+	        $data = array();
+
+	        foreach($user_reports as $report){
+	        	//checking if this is the report of this manager or not
+	        	if($report->role == 'manager' && $report->user_id != $user_id){
+	        		continue;
+	        	}
+	        	//get the year month and day of the report deadline
+	        	$date = explode(' ', $report->deadline);
+	        	$date = explode('-', $date[0]);
+	        	$report_year = $date[0];
+	        	$report_month = $date[1];
+	        	$report_day = $date[2];
+
+	        	if($year == $report_year && $month == $report_month){
+	        		$content = 'Not Submitted';
+	        		if($report->submit_date != NULL){
+	        			//formatting the date to dd-mm-yyyy
+	        			$report_submit_date = explode(' ', $report->submit_date);
+	        			$submit_date = explode('-', $report_submit_date[0]);
+	        			$submit_date = $submit_date[2].'-'.$submit_date[1].'-'.$submit_date[0];
+	        			$report_submit_date = $submit_date.' '.$report_submit_date[1];
+
+ 	        			$content = 'submitted on '.$report_submit_date;
+	        		}
+	        		$report_name = '';
+	        		if($user_id == $report->user_id){
+	        			$report_name = $report->report_name;
+ 	        		}else{
+ 	        			$report_name = $report->report_name.' - '.$report->name;
+ 	        		}
+
+ 	        		//check if there is an existing date or not
+	        		if(array_key_exists($report_day,$data)){
+	        			array_push($data[$report_day],'<a class="content" data-toggle="tooltip" data-placement="top" title="'.$content.'" style="background-color:'.$report->report_color.'" href="">'.$report_name.'</a>');
+	        			
+
+	        		}else{
+	        			$data[$report_day] = array('<a class="content" data-toggle="tooltip" data-placement="top" title="'.$content.'" style="background-color:'.$report->report_color.'" href="">'.$report_name.'</a>');
+	        		}
+
+	        		
+	        	}
+
+
+	        }
+	       
+
+	        $data_view = array();
+
+	        if($data){
+	        	foreach ($data as $day => $value) {
+	        		$view = implode(' ',$value);
+	        		$data_view[$day] = $view; 
+	        	}
+	        }
+
+	        
+
+	       
+
+	        return $this->calendar->generate($year,$month,$data_view);
+
+		}
+
+		//generate calendar for admin
 		function admin_generate($year,$month,$company_id){
 
 	        $this->load->library('calendar',$this->conf);
@@ -116,6 +198,7 @@
 
 		}
 
+		//generate calendar for staff
 		function generate($year,$month,$user_id){
 			
 
--- application/views/templates/default.php
+++ application/views/templates/default.php
@@ -164,6 +164,7 @@
       $active_photo = base_url().$active_user->photo; 
     }
     
+    $role = $this->crud_model->get_by_condition('users',array('id' => $this->session->userdata('is_active')))->row('role');
    ?>
 
 
@@ -174,14 +175,24 @@
         <div class="company_logo" style="display:none;margin-bottom: 10px; padding-left: 15px; padding-bottom:20px; border-bottom:7px solid #000">
           <img src="<?php echo base_url()?>assets/william_platform_logo.png" width="100px" style="margin-left:15%;">
         </div>
+
         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()" style="border-bottom: none;font-size:25px!important; display:none">&times;</a>
+
         <a href="<?php echo base_url('main') ?>"><i class="fa fa-home" aria-hidden="true"></i><span class="menu-text pull-right" style="display:none">Home</span></a>
+        <?php if($role != 'admin'): ?>
         <a href="<?php echo base_url('form/submit_report') ?>" ><i id="submit-button" class="fa fa-hand-pointer-o" aria-hidden="true"></i><span class="menu-text pull-right" style="display:none">Submit</span></a>
+        <?php endif; ?>
+
+        <?php if($role != 'staff'): ?>
         <a href="<?php echo base_url('templates') ?>" ><i id="template-button" class="fa fa-file-text" aria-hidden="true"></i><span class="menu-text pull-right" style="display:none">Template</span></a>
+        <?php endif; ?>
+
         <a href="<?php echo base_url('user/settings/'.$this->session->userdata('is_active')) ?>" ><i id="setting-button" class="fa fa-cogs" aria-hidden="true"></i><span class="menu-text pull-right" style="display:none">Settings</span></a>
 
         <a href="<?php echo base_url('main/faq') ?>"><i class="fa fa-question" aria-hidden="true"></i><span class="menu-text pull-right" style="display:none">FAQ and How</span></a>
+
         <a href="<?php echo base_url('') ?>"><i class="fa fa-headphones" id="service-button" aria-hidden="true"></i><span class="menu-text pull-right" style="display:none">Customer Service</span></a>
+
         <div class="bottom-align-text-2 text-center">Williams Business Solutions Reporting System V1.0</div>
         <div class="bottom-align-text text-center">For help and feedback contact office@williamsbusiness.co.nz</div>
       </div>
